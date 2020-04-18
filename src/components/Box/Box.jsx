977aae431aeb04ce552ec49daf9ce78170334dae
Kenold Beauplan <kenold@users.noreply.github.com>
2020-04-18 18:01:15 -0400

    add html markup

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Box = ({ title, subtitle, label, image }) => {
-  return <div className="box">Box</div>;
+  return (
+    <div className="box">
+      <div className="box__label">label</div>
+      <h4 className="box__title">title</h4>
+      <div className="box__subtitle">sub</div>
+      <div className="box__icon">icon</div>
+      <div className="box__image">image</div>
+    </div>
+  );
 };
 
 export default Box;
